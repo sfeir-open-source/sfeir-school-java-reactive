995ece3d2056b2455529e93193b980516beffc4b
Sylvain Magnier <magnier.s@sfeir.com>
2023-08-23 13:44:46 +0200

    worshop(init): init all workshops
    - `basics` workshop for introduction to common objects and operation with Reactor
    - `errors` workshop for section about errors handling and logging
    - `advanced` workshop about backpressure, executor, complexer use cases

---
+++ steps/01-reactor-basics-solution/src/test/java/com/sfeir/schools/java/reactorbasics/ReactorBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfeir.schools.java.reactorbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ steps/02-reactor-errors-solution/src/test/java/com/sfeir/schools/java/reactorerrors/ReactorErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfeir.schools.java.reactorerrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorErrorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
