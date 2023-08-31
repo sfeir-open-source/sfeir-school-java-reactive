4d186f17bce8ab4023afa8a248409464ead9b53a
nathan-sfeir <pecqueux.n@sfeir.com>
2023-08-31 15:17:34 +0200

    doc(workshop): first test create flux

--- steps/01-reactor-basics-solution/src/main/java/com/sfeir/schools/java/reactorbasics/ReactorBasicsApplication.java
+++ steps/01-reactor-basics-solution/src/main/java/com/sfeir/schools/java/reactorbasics/ReactorBasicsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactorBasicsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReactorBasicsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ReactorBasicsApplication.class, args);
+  }
 
 }
