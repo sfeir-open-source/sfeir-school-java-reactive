576b2b9578259bad5522abc07980a4f3cb61b644
nathan-sfeir <pecqueux.n@sfeir.com>
2023-09-16 14:45:12 +0200

    feat(workshop): delete third workshop + create last exercise error

--- steps/02-reactor-errors-solution/src/main/java/com/sfeir/schools/java/reactorerrors/commons/Workshop02Errors.java
+++ steps/02-reactor-errors-solution/src/main/java/com/sfeir/schools/java/reactorerrors/commons/Workshop02Errors.java
@@ -6,13 +6,14 @@ import com.sfeir.schools.java.reactorerrors.commons.domain.Shape;
 import com.sfeir.schools.java.reactorerrors.commons.services.ColorProvider;
 import com.sfeir.schools.java.reactorerrors.commons.services.FigureProvider;
 import com.sfeir.schools.java.reactorerrors.commons.services.ShapeProvider;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
-import java.util.List;
-import java.util.Random;
+import java.time.Duration;
 
-public class WorkshopErrors {
+@Slf4j
+public class Workshop02Errors {
 
   public ColorProvider colorProvider;
   public ShapeProvider shapeProvider;
@@ -39,7 +40,7 @@ public class WorkshopErrors {
    * Dans ce cas retourner le flux default
    * @return Flux<Color>
    */
-  public Flux<Shape> onErrorResumeFlux() {
+  public Flux<Shape> onErrorReturnDefaultFlux() {
     Flux<Shape> defaultShapesFlux = Flux.just(Shape.SQUARE,Shape.SQUARE,Shape.SQUARE,Shape.SQUARE,Shape.SQUARE);
 
     // Appelle la méthode getRandomError pour obtenir un flux de shaoes aléatoires.
@@ -54,13 +55,42 @@ public class WorkshopErrors {
   }
 
   /**
-   *
+   * Appeler la méthode getFiveFiguresAfterOneError()
+   * si elle génére une erreur, relancé la
    * @return Flux<Figure>
    */
-  public Flux<Figure> return5FigureAfterRetry() {
-    return figureProvider.get5FiguresWithConditionalError()
+  public Flux<Figure> onErrorRetryOneTime() {
+    return figureProvider.getFiveFiguresAfterOneError()
       .retry(1); // Relancer une fois en cas d'erreur
   }
 
+  /**
+   * Appeler la méthode getFiveFiguresAfterOneError()
+   * si elle génère une erreur, attendez 3 secondes puis relancez la
+   * @return Flux<Figure>
+   */
+  public Flux<Figure> onErrorRetryAfterOneSecond() {
+    return figureProvider.getFiveFiguresAfterOneError()
+      .retryWhen(Retry.backoff(1, Duration.ofSeconds(1)));
+  }
 
+  /**
+   * Appeler la méthode getFiveFiguresAfterOneError()
+   * si elle génère une erreur, retourner une CustomException contenant "Erreur sur le flux de figures"
+   * @return Flux<Figure>
+   */
+  public Flux<Figure> onErrorReturnCustomException() {
+    return figureProvider.getFiveFiguresAfterOneError()
+      .onErrorMap(error -> new Exception("Erreur sur le flux de figures", error));
+  }
+
+  /**
+   * Appeler la méthode getFiveFiguresAfterOneError()
+   * si elle génère une erreur, afficher simplement une erreur dans les logs "Erreur rencontrée lors de l'émission des figures"
+   * @return Flux<Figure>
+   */
+  public Flux<Figure> onErrorPrintLogError() {
+    return figureProvider.getFiveFiguresAfterOneError()
+      .doOnError(e -> log.error("Erreur rencontrée", e));
+  }
 }
---
+++ steps/02-reactor-errors-solution/src/test/java/com/sfeir/schools/java/reactorerrors/Workshop02ErrorsTest.java
@@ -0,0 +1,115 @@
+package com.sfeir.schools.java.reactorerrors;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import com.sfeir.schools.java.reactorerrors.commons.Workshop02Errors;
+import com.sfeir.schools.java.reactorerrors.commons.domain.Color;
+import com.sfeir.schools.java.reactorerrors.commons.domain.Figure;
+import com.sfeir.schools.java.reactorerrors.commons.domain.Shape;
+import com.sfeir.schools.java.reactorerrors.commons.services.ColorProvider;
+import com.sfeir.schools.java.reactorerrors.commons.services.FigureProvider;
+import com.sfeir.schools.java.reactorerrors.commons.services.ShapeProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+public class Workshop02ErrorsTest {
+
+  ColorProvider colorProvider;
+  ShapeProvider shapeProvider;
+  FigureProvider figureProvider;
+  private Workshop02Errors workshop02errors;
+
+  @BeforeEach
+  public void setUp() {
+    colorProvider = new ColorProvider();
+    shapeProvider = new ShapeProvider();
+    figureProvider = new FigureProvider();
+    workshop02errors = new Workshop02Errors();
+    workshop02errors.colorProvider = colorProvider;
+    workshop02errors.shapeProvider = shapeProvider;
+    workshop02errors.figureProvider = figureProvider;
+  }
+
+  @Test
+  public void testOnErrorReturnRed() {
+    Flux<Color> colorFlux = workshop02errors.onErrorReturnRed();
+    StepVerifier.create(colorFlux)
+      .expectNextCount(3)
+      .expectNext(Color.RED)
+      .expectComplete()
+      .verify();
+  }
+
+  @Test
+  public void testOnErrorReturnDefaultFlux_FiveSquareFlux() {
+    List<Shape> defaultShapesFlux = Arrays.asList(Shape.SQUARE, Shape.SQUARE, Shape.SQUARE, Shape.SQUARE, Shape.SQUARE);
+
+    Flux<Shape> shapeFlux = workshop02errors.onErrorReturnDefaultFlux();
+    StepVerifier.create(shapeFlux)
+      .expectNextCount(3)
+      .expectNextSequence(defaultShapesFlux)
+      .expectComplete()
+      .verify();
+  }
+
+  @Test
+  public void testOnErrorRetryOneTime_returnAfterRetry() {
+    Flux<Figure> figuresFlux = workshop02errors.onErrorRetryOneTime();
+
+    StepVerifier.create(figuresFlux)
+      .expectNextCount(8)
+      .expectComplete()
+      .verify(Duration.ofSeconds(1)); // Vérifie que le flux se termine en moins de 1 seconde
+  }
+
+  @Test
+  public void testOnErrorRetryAfterOneSecond_returnDurationOneSec() {
+    Flux<Figure> figuresFlux = workshop02errors.onErrorRetryAfterOneSecond();
+
+    StepVerifier.create(figuresFlux)
+      .expectNextCount(8)
+      .expectComplete()
+      .verify(Duration.ofSeconds(2)); // Vérifie que le flux se termine en moins de 1 seconde
+  }
+
+  @Test
+  public void testOnErrorReturnCustomException_returnCorrectException() {
+    Flux<Figure> figuresFlux = workshop02errors.onErrorReturnCustomException();
+
+    StepVerifier.create(figuresFlux)
+      .expectNextCount(3) // Le flux devrait émettre 4 éléments avant de générer une exception
+      .expectErrorMatches(error -> error instanceof Exception &&
+        "Erreur sur le flux de figures".equals(error.getMessage()))
+      .verify();
+  }
+
+  @Test
+  public void testOnErrorPrintLogError_printCorrectLog() {
+    Flux<Figure> figuresFlux = workshop02errors.onErrorPrintLogError();
+
+    Logger logger = (Logger) LoggerFactory.getLogger(Workshop02Errors.class);
+    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.addAppender(listAppender);
+
+    StepVerifier.create(figuresFlux)
+      .expectNextCount(3) // Le flux devrait émettre 4 éléments avant de générer une exception
+      .expectError(RuntimeException.class)
+      .verify();
+
+    boolean hasLoggedError = listAppender.list.stream()
+      .anyMatch(event -> event.getLevel().equals(Level.ERROR) &&
+        "Erreur rencontrée".equals(event.getFormattedMessage()));
+
+    assert(hasLoggedError);
+  }
+}
