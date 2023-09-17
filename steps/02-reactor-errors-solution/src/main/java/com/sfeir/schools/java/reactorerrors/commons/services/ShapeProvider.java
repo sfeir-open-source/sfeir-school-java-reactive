d410a491b009ef3181317a9ac3a54efb37a9a03f
nathan-sfeir <pecqueux.n@sfeir.com>
2023-09-17 11:14:38 +0200

    feat(workshop): add one advanced exercise error

--- steps/02-reactor-errors-solution/src/main/java/com/sfeir/schools/java/reactorerrors/commons/services/ShapeProvider.java
+++ steps/02-reactor-errors-solution/src/main/java/com/sfeir/schools/java/reactorerrors/commons/services/ShapeProvider.java
@@ -1,11 +1,16 @@
 package com.sfeir.schools.java.reactorerrors.commons.services;
 
+import com.sfeir.schools.java.reactorerrors.commons.domain.Color;
+import com.sfeir.schools.java.reactorerrors.commons.domain.Figure;
 import com.sfeir.schools.java.reactorerrors.commons.domain.Shape;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.random.RandomGenerator;
 
 import static com.sfeir.schools.java.reactorerrors.commons.domain.Shape.*;
+@Slf4j
 
 public class ShapeProvider {
 
@@ -13,6 +18,7 @@ public class ShapeProvider {
     return Flux.just(CIRCLE, SQUARE, SQUARE, TRIANGLE);
   }
 
+  private final AtomicBoolean hasThrown = new AtomicBoolean(false);
 
   public Flux<Shape> getRandomError(int sizeLimit) {
     return Flux
@@ -43,5 +49,24 @@ public class ShapeProvider {
     int randomIndex = RandomGenerator.getDefault()
       .nextInt(Shape.values().length);
     return Shape.values()[randomIndex];
+  }
+
+  public Flux<Shape> getFiveShapesAfterOneError() {
+    log.error("test");
+    return Flux.create(sink -> {
+      for (int i = 0; i < 5; i++) {
+        if (i == 3) { // Générer une exception au 4e élément seulement lors du premier appel
+          sink.error(new RuntimeException());
+          return;
+        }
+
+        // Générer une figure aléatoire
+        Shape shape = randomShape();
+
+        // Émettre la shape
+        sink.next(shape);
+      }
+      sink.complete();
+    });
   }
 }
