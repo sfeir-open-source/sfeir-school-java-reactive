4ed1b3ee696b322a392151abe5d9e2dfdff28b4e
Sylvain Magnier <magnier.s@sfeir.com>
2023-09-20 18:07:18 +0200

    workshop(operators): added exercises about combination operators

---
+++ steps/01-reactor-basics/src/main/java/com/sfeir/schools/java/reactorbasics/commons/Workshop01FluxCombine.java
@@ -0,0 +1,66 @@
+package com.sfeir.schools.java.reactorbasics.commons;
+
+import com.sfeir.schools.java.reactorbasics.commons.domain.Color;
+import com.sfeir.schools.java.reactorbasics.commons.domain.Figure;
+import com.sfeir.schools.java.reactorbasics.commons.domain.Shape;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static com.sfeir.schools.java.reactorbasics.commons.domain.Color.*;
+import static com.sfeir.schools.java.reactorbasics.commons.domain.Shape.*;
+
+public class Workshop01FluxCombine {
+
+  /**
+   * on a 2 flux, le premier étant retardé
+   * Il faut étendre le premier avec le 2e
+   * Le plus rapide sortira de toute façon en premier
+   */
+  static Flux<Color> mergeFluxOfColor() {
+    Flux<Color> firstColors = Flux.just(RED, GREEN, GREEN, BLUE).delayElements(Duration.ofMillis(2));
+    Flux<Color> secondColors = Flux.just(YELLOW, RED, BLUE, BLUE);
+
+    return firstColors;
+  }
+
+  /**
+   * on a 2 flux, le premier étant retardé
+   * Il faut étendre le premier avec le 2e
+   * L'opérateur respecte strictement l'ordre des flux
+   */
+  static Flux<Color> concatFluxOfColors() {
+    Flux<Color> firstColors = Flux.just(RED, GREEN, GREEN, BLUE).delayElements(Duration.ofMillis(2));
+    Flux<Color> secondColors = Flux.just(YELLOW, RED, BLUE, BLUE);
+
+    return firstColors;
+  }
+
+  /**
+   * à partir d'un flux de couleur et un autre de formes
+   * Il faut combiner le 2nd au 1er, et transformer les pairs de valeurs en objet Figure
+   */
+  static Flux<Figure> zipWithShapeAndColorsIntoFigure() {
+    // les flux à combiner pour pouvoir créer des Figure à partir de pairs de valeur
+    Flux<Color> colors = Flux.just(RED, GREEN, GREEN, YELLOW, RED, BLUE);
+    Flux<Shape> shapes = Flux.just(CIRCLE, TRIANGLE, TRIANGLE, SQUARE, SQUARE, TRIANGLE);
+
+    // utiliser l'opérateur zipWith pour manipuler un Color et un Shape à la fois
+    return Flux.empty();
+  }
+
+  /**
+   * à partir d'un flux de couleur et un autre de formes
+   * Il faut appairer les 2 flux pour combiner les pairs de valeurs en objet Figure
+   * Cette fois utiliser la méthode static de la classe Flux
+   */
+  static Flux<Figure> zipShapeAndColorsIntoFigure() {
+    // les flux à combiner pour pouvoir créer des Figure à partir de pairs de valeur
+    Flux<Color> colors = Flux.just(RED, GREEN, GREEN, YELLOW, RED, BLUE);
+    Flux<Shape> shapes = Flux.just(CIRCLE, TRIANGLE, TRIANGLE, SQUARE, SQUARE, TRIANGLE);
+
+    // utiliser l'opérateur static Flux.zip pour combiner les flux et manipuler un Color et un Shape à la fois
+    return Flux.empty();
+  }
+
+}
---
+++ steps/01-reactor-basics/src/test/java/com/sfeir/schools/java/reactorbasics/commons/Workshop01FluxCombineTest.java
@@ -0,0 +1,77 @@
+package com.sfeir.schools.java.reactorbasics.commons;
+
+import com.sfeir.schools.java.reactorbasics.commons.domain.Color;
+import com.sfeir.schools.java.reactorbasics.commons.domain.Figure;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static com.sfeir.schools.java.reactorbasics.commons.domain.Color.*;
+import static com.sfeir.schools.java.reactorbasics.commons.domain.Shape.*;
+
+class Workshop01FluxCombineTest {
+
+  /*
+    MERGEWITH
+   */
+  // merge, ordre non garantie
+    @Test
+    void testMergeFluxOfColor() {
+      Flux<Color> result = Workshop01FluxCombine.mergeFluxOfColor();
+
+      StepVerifier.create(result)
+        .expectNext(YELLOW, RED, BLUE, BLUE, RED, GREEN, GREEN, BLUE)
+        .verifyComplete();
+    }
+
+  /*
+    CONCATWITH
+   */
+
+  //concat des flux, vérifier ordre
+    @Test
+    void testConcatFluxOfColors() {
+      Flux<Color> result = Workshop01FluxCombine.concatFluxOfColors();
+
+      StepVerifier.create(result)
+        .expectNext(RED, GREEN, GREEN, BLUE, YELLOW, RED, BLUE, BLUE)
+        .verifyComplete();
+    }
+
+  /*
+    ZIPWITH
+   */
+
+  // zip un flux de Shape et un flux de Color, produit un flux de Figure
+  @Test
+  void testZipWithShapeAndColorsIntoFigure() {
+    Flux<Figure> result = Workshop01FluxCombine.zipWithShapeAndColorsIntoFigure();
+
+    StepVerifier.create(result)
+      .expectNext(
+        new Figure(CIRCLE, RED),
+        new Figure(TRIANGLE, GREEN),
+        new Figure(TRIANGLE, GREEN),
+        new Figure(SQUARE, YELLOW),
+        new Figure(SQUARE, RED),
+        new Figure(TRIANGLE, BLUE)
+      )
+      .verifyComplete();
+  }
+
+  @Test
+  void testZipShapeAndColorsIntoFigure() {
+    Flux<Figure> result = Workshop01FluxCombine.zipShapeAndColorsIntoFigure();
+    StepVerifier.create(result)
+      .expectNext(
+        new Figure(CIRCLE, RED),
+        new Figure(TRIANGLE, GREEN),
+        new Figure(TRIANGLE, GREEN),
+        new Figure(SQUARE, YELLOW),
+        new Figure(SQUARE, RED),
+        new Figure(TRIANGLE, BLUE)
+      )
+      .verifyComplete();
+  }
+
+}
