5bfc0a2dff9c2bc9894f39caf5e266ce299b9ce0
Sylvain Magnier <magnier.s@sfeir.com>
2023-09-22 11:02:53 +0200

    workshop(operators): fix tests about transformation workshop

--- steps/01-reactor-basics/src/test/java/com/sfeir/schools/java/reactorbasics/commons/Workshop01FluxTransformTest.java
+++ steps/01-reactor-basics/src/test/java/com/sfeir/schools/java/reactorbasics/commons/Workshop01FluxTransformTest.java
@@ -3,6 +3,7 @@ package com.sfeir.schools.java.reactorbasics.commons;
 import com.sfeir.schools.java.reactorbasics.commons.domain.Color;
 import com.sfeir.schools.java.reactorbasics.commons.domain.Figure;
 import com.sfeir.schools.java.reactorbasics.commons.domain.Shape;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
@@ -10,6 +11,7 @@ import reactor.core.publisher.GroupedFlux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Collections;
 import java.util.List;
 
 import static com.sfeir.schools.java.reactorbasics.commons.domain.Color.*;
@@ -73,16 +75,25 @@ class Workshop01FluxTransformTest {
   void testGroupByFirstChar() {
     Flux<GroupedFlux<Character, String>> result= Workshop01FluxTransform.groupByFirstChar();
 
-    StepVerifier.create(result)
-      .expectNextMatches(
-        group -> group.key() == 'B'
-          && List.of("Big").equals(group.collectList().block()))
-      .expectNextMatches(
-        group -> group.key() == 'H'
-          && List.of("Handle", "Here").equals(group.collectList().block()))
-      .expectNextMatches(
-        group -> group.key() == 'S'
-          && List.of("Shoulder", "Something").equals(group.collectList().block()));
+    List<GroupedFlux<Character, String>> collected = result.collectList().block();
+    Assertions.assertNotNull(collected);
+    Assertions.assertTrue(collected.size() > 0, "Collected content should not be empty");
+
+    Mono<List<String>> hCollection = collected.get(0).collectList();  // first signal is H group, not B
+    Mono<List<String>> bCollection = collected.get(1).collectList();
+    Mono<List<String>> sCollection = collected.get(2).collectList();
+
+    StepVerifier.create(bCollection)
+      .expectNext(List.of("Big"))
+      .verifyComplete();
+
+    StepVerifier.create(hCollection)
+      .expectNext(List.of("Handle", "Here"))
+      .verifyComplete();
+
+    StepVerifier.create(sCollection)
+      .expectNext(List.of("Shoulder", "Something"))
+      .verifyComplete();
   }
 
   // grouper Figures par couleur
@@ -90,16 +101,26 @@ class Workshop01FluxTransformTest {
   void testGroupByFigureColor() {
     Flux<GroupedFlux<Color, Figure>> result = Workshop01FluxTransform.groupByFigureColor();
 
-    StepVerifier.create(result)
-      .expectNextMatches(
-        group -> group.key() == GREEN
-          && List.of(new Figure(SQUARE, GREEN), new Figure(SQUARE, GREEN)).equals(group.collectList().block()))
-      .expectNextMatches(
-        group -> group.key() == BLUE
-          && List.of(new Figure(CIRCLE, BLUE), new Figure(TRIANGLE, BLUE)).equals(group.collectList().block()))
-      .expectNextMatches(
-        group -> group.key() == RED
-          && List.of(new Figure(CIRCLE, RED), new Figure(CIRCLE, RED)).equals(group.collectList().block()));
+    List<GroupedFlux<Color, Figure>> collected = result.collectList().block();
+    Assertions.assertNotNull(collected);
+    Assertions.assertTrue(collected.size() > 0, "Collected content should not be empty");
+
+
+    Mono<List<Figure>> greenCollection = collected.get(0).collectList();
+    Mono<List<Figure>> blueCollection = collected.get(1).collectList();
+    Mono<List<Figure>> redCollection = collected.get(2).collectList();
+
+    StepVerifier.create(greenCollection)
+      .expectNext(List.of(new Figure(SQUARE, GREEN), new Figure(SQUARE, GREEN)))
+      .verifyComplete();
+
+    StepVerifier.create(blueCollection)
+      .expectNext(List.of(new Figure(CIRCLE, BLUE), new Figure(TRIANGLE, BLUE)))
+      .verifyComplete();
+
+    StepVerifier.create(redCollection)
+      .expectNext(List.of(new Figure(CIRCLE, RED), new Figure(CIRCLE, RED)))
+      .verifyComplete();
   }
 
   // grouper Figures par forme
